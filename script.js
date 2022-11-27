fc04150afd201e961323ce84fec437449079b8c7
He-zekiah <hezekiahd58@gmail.com>
2022-11-26 23:00:29 -0700

    active

--- script.js
+++ script.js
@@ -5,33 +5,70 @@ const info2 = document.getElementById('info2');
 const refresh = document.getElementById('reset');
 let amount = document.getElementById("amount");
 let total = document.getElementById("total");
+let billAmt = document.getElementById('billAmt');
+let people = document.getElementById('numPple');
 let bill;
 let numOfPeople;
 
-info.addEventListener("change", function (e) {
-  bill =  e.target.value.trim();
-});
-console.log(bill);
+// check for minimum and maximum character
+// const isBetween = (length, max) => length < max ? false : true;
 
-info2.addEventListener("change", function(e) {
+info.addEventListener("change", function (e) {
+    bill = e.target.value.trim();
+});
+
+info2.addEventListener("change", function (e) {
     numOfPeople = e.target.value.trim();
 });
-
-
 
 const calculateTip = (num) => {
     const tipPerPerson = parseFloat((num / 100) * parseInt(bill));
     const totalPerPerson = (num / 100) * (parseInt(bill) * parseInt(numOfPeople));
-    console.log(tipPerPerson, totalPerPerson, num);
-    amount.innerText = `${tipPerPerson}.00` || `$0.00`;
-    total.innerText = `${totalPerPerson}.00` || `$0.00`;
+    amount.innerText = isNaN(tipPerPerson) ? `$0.00` : `$${(tipPerPerson).toFixed(2)}`;
+    total.innerText = isNaN(totalPerPerson) ? `$0.00` : `$${(totalPerPerson).toFixed(2)}`;
 }
 
-btns.forEach((btn) => {
-btn.addEventListener("click", function () {
-    const num = parseInt(btn.innerText.replace(/%/g, ""));
-    calculateTip(num);
+const checkError = () => {
+    if (info.value.trim() === '') {
+        billAmt.innerText = "Can't be blank";
+        info.classList.add('error');
+    } else {
+        billAmt.innerText = '';
+        info.classList.remove('error');
+    };
+    if (info2.value.trim() === '') {
+        people.innerText = "Can't be blank";
+        info2.classList.add('error');
+    } else {
+        people.innerText = '';
+        info2.classList.remove('error');
+    };
+};
 
-})
+btns.forEach((btn) => {
+    btn.addEventListener("click", function () {
+        checkError();
+        const num = parseInt(btn.innerText.replace(/%/g, ""));
+        calculateTip(num);
+
+    })
+});
+
+option.addEventListener('input', (e) => {
+    checkError();
+    let num = e.target.value.trim();
+    calculateTip(num);
+});
+
+refresh.addEventListener('click', () => {
+    amount.innerText = `$0.00`;
+    total.innerText = `$0.00`;
+    info.value = '';
+    info2.value = '';
+    info.classList.remove('error');
+    info2.classList.remove('error');
+    billAmt.innerText = '';
+    people.innerText = '';
+    option.value = '';
 });
 
